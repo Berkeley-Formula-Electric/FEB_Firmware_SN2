5954c5fe36e80fd0f0cd19fab36ef5a257d48b25
Aman Doshi <amandoshi001@gmail.com>
2023-01-22 21:02:27 -0800

    initial commit

---
+++ BMS_Temperature_LUT/interpolate.py
@@ -0,0 +1,81 @@
+import csv
+
+# constants
+CSV_FILE_PATH = "values.csv"
+C_HEADER_FILE_PATH = "temp_volt_map.h"
+RESOLUTION = 0.1
+LOWER_TEMP = -40
+UPPER_TEMP = 120
+
+# test
+CSV_NEW_DATA_FILE_PATH = "result.csv"
+
+def read_csv(file_path):
+    assert ".csv" in file_path
+    
+    with open(file_path) as csv_file:
+        reader = csv.reader(csv_file, delimiter=",", quotechar='"')
+        return list(reader)
+
+def interpolate(x, x1, y1, x2, y2):
+    return y1 + (x - x1) * (y2 - y1) / (x2 - x1)
+
+def interpolate_data(temp_voltage_data):
+    result = []
+    for i in range(len(temp_voltage_data) - 1):
+        x1 = temp_voltage_data[i][0]
+        y1 = temp_voltage_data[i][1]
+        x2 = temp_voltage_data[i + 1][0]
+        y2 = temp_voltage_data[i + 1][1]
+
+        x = x1
+        while x < x2:
+            y = interpolate(x, x1, y1, x2, y2)
+            result.append([round(x, 4), round(y, 4)])
+            x += RESOLUTION
+    
+    result.append(temp_voltage_data[-1][:])
+
+    return result
+
+def write_csv(file_path, data_arr):
+    with open(file_path, "w") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerows(data_arr)
+
+def write_c_header(file_path, str):
+    with open(file_path, "w") as c_header_file:
+        c_header_file.write(str)
+
+def arr_to_c_arr_string(temp_voltage_map):
+    def map_arr_cArr(arr):
+        if not isinstance(arr, list):
+            return str(arr)
+        
+        return "{" + ", ".join(map(map_arr_cArr, arr)) + "}"
+    
+    result_arr_str = []
+    result_arr_str.append(f"#define MAP_COUNT {len(temp_voltage_map)}")
+    result_arr_str.append(f"float TEMP_VOLT_MAP[][2] = {map_arr_cArr(temp_voltage_map)};")
+    result_arr_str.append(f"float RESOLUTION = {RESOLUTION};")
+    result_arr_str.append(f"float MIN_MAP_TEMP = {temp_voltage_map[0][0]};")
+    result_arr_str.append(f"float MAX_MAP_TEMP = {temp_voltage_map[-1][0]};")
+    result_arr_str.append(f"float MIN_MAP_VOLT = {temp_voltage_map[-1][1]};")
+    result_arr_str.append(f"float MAX_MAP_VOLT = {temp_voltage_map[0][1]};")
+
+    return "\n".join(result_arr_str)
+
+def main():
+    csv_data = read_csv(CSV_FILE_PATH)[1:]
+    temp_voltage_data = sorted(list(map(lambda x : [float(x[0]), float(x[1])], csv_data)))    
+    temp_voltage_LUT = interpolate_data(temp_voltage_data)
+
+    # test
+    write_csv(CSV_NEW_DATA_FILE_PATH, temp_voltage_LUT)
+
+    c_arr_string = arr_to_c_arr_string(temp_voltage_LUT)
+
+    write_c_header(C_HEADER_FILE_PATH, c_arr_string)
+
+if __name__ == "__main__":
+    main()
