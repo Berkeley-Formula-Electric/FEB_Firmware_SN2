f4574013985c7f46ffa04b3f30431d5f103ad268
Wenhao Li <wli06@berkeley.edu>
2023-05-30 15:58:47 -0700

    added buttons for peripheral devices

--- STEERING_WHEEL/Core/Src/main.c
+++ STEERING_WHEEL/Core/Src/main.c
@@ -191,34 +191,34 @@ int main(void)
 //	  }
 
 	  if (!(data2 & (1<<1))) { // BUT_4 -> IO2 P1, coolant pump
-		  if(!lastButton_4) { // if it is not pressed last time, record time of change
-			  last_debounce_time_4 = HAL_GetTick();
-			  lastButton_4 = true;
-		  }
-		  if((HAL_GetTick() - last_debounce_time_4 > debouce_delay) && lastButton_4) {
+		  if (!lastButton_4) {
+			  lock_4 = true;
 			  coolant_pump = !coolant_pump;
 			  FEB_CAN_Transmit(&hcan1, SW_COOLANT_PUMP, (uint8_t *) &coolant_pump, 1);
 		  }
+		  lastButton_4 = true;
 	  } else {
+		  if (lastButton_4) {
+			  lock_4 = false;
+		  }
 		  lastButton_4 = false;
 	  }
 
 	  if (!(data2 & (1<<2))) { // BUT_5 -> IO2 P2, accumulator fans
-		  if(!lastButton_5) { // if it is not pressed last time, record time of change
-			  last_debounce_time_5 = HAL_GetTick();
-			  lastButton_5 = true;
-		  }
-		  if((HAL_GetTick() - last_debounce_time_5 > debouce_delay) && lastButton_5) {
+		  if (!lastButton_5) {
+			  lock_5 = true;
 			  accumulator_fans = !accumulator_fans;
 			  FEB_CAN_Transmit(&hcan1, SW_ACUMULATOR_FANS, (uint8_t *) &accumulator_fans, 1);
 		  }
+		  lastButton_5 = true;
 	  } else {
+		  if (lastButton_5) {
+			  lock_5 = false;
+		  }
 		  lastButton_5 = false;
 	  }
 
 	  if (!(data2 & (1<<3))) { // BUT_6 -> IO2 P3, extra
-		  buf_len = sprintf((char*)buf, "BUT_2\r\n");
-		  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, HAL_MAX_DELAY);
 		  if (!lastButton_6) {
 			  lock_6 = true;
 			  extra = !extra;
@@ -280,8 +280,8 @@ int main(void)
 	  }
 //	  buf_len = sprintf((char*)buf, "check:%d flag:%d\r\n", Button_Checking, Button_Timer_Flag);
 //	  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, HAL_MAX_DELAY);
-//	  buf_len = sprintf((char*)buf, "ready: %d coolant: %d accumulator: %d extra: %d\r\n", ready_to_drive, coolant_pump, accumulator_fans, extra);
-//	  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, HAL_MAX_DELAY);
+	  buf_len = sprintf((char*)buf, "ready: %d coolant: %d accumulator: %d extra: %d\r\n", ready_to_drive, coolant_pump, accumulator_fans, extra);
+	  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, HAL_MAX_DELAY);
 
 
 
