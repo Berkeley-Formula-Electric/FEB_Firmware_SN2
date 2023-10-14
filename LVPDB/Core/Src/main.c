df5a74d4dafdf76e01e82afc85c59ba9965cada5
nidhir <nidhirg2014@gmail.com>
2023-10-13 17:37:15 -0700

    printing tps readings

--- LVPDB/Core/Src/main.c
+++ LVPDB/Core/Src/main.c
@@ -161,6 +161,8 @@ int main(void)
 	char buf[128];
 	int buf_len;
 	float current_reading;
+	float ex_current_reading;
+	float cp_current_reading;
 
   /* USER CODE END 2 */
 
@@ -212,11 +214,14 @@ int main(void)
 	  FEB_TPS2482_shutdownIfError(hi2c1p, EX_ADDR, GPIOC, GPIO_PIN_3, GPIOC, GPIO_PIN_1, GPIOC, GPIO_PIN_2, 22.5, 25.5, 7, 4, 150, 120);
 ***/
 	  current_reading = FEB_TPS2482_PollBusCurrent(hi2c1p,LV_ADDR);
+	  ex_current_reading = FEB_TPS2482_PollBusCurrent(hi2c1p,EX_ADDR);
+	  cp_current_reading = FEB_TPS2482_PollBusCurrent(hi2c1p,CP_ADDR);
+
 
 	  buf_len = sprintf((char*)buf, "ready: %d, brake: %.3f\r\n", SW_MESSAGE.ready_to_drive, APPS_MESSAGE.brake_pedal);
-	  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, HAL_MAX_DELAY);
+//	  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, HAL_MAX_DELAY);
 
-	  buf_len = sprintf((char*) buf, "Current Draw: %.3f\r\n", current_reading);
+	  buf_len = sprintf((char*) buf, "Current Draw (LV, EX, CP): %.3f, %.3f, %.3f\r\n", current_reading, ex_current_reading, cp_current_reading);
 	  HAL_UART_Transmit(&huart2, (uint8_t *)buf, buf_len, HAL_MAX_DELAY);
     /* USER CODE END WHILE */
 
